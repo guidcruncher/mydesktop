346eb9363321f4137c613665908fba64441999d2
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-12-18 21:21:07 +0000

    fx

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+const isMenuOpen=ref(false)
+</script>
 
 <template>
   <UINavigationBar title="Desktop">
-    <template #left> </template>
+    <template #left>
+            <UIDropdownMenu v-model="isMenuOpen">
+              <template #trigger="{ isOpen }">
+                <UIDropdownMenuTrigger
+                  class="control-btn"
+                  icon="fa-solid fa-grip-lines"
+                  title="Desk"
+                  :active="isOpen"
+                />
+              </template>
+              <UIDropdownMenuItem
+                icon="fa-solid fa-redo"
+                label="Reset All"
+                title="Reset All (R)"
+                @click="resetManipulation"
+              />
+              <hr class="menu-divider" />
+              <UIDropdownMenuItem
+                :active="isGrayscale"
+                icon="fa-solid fa-palette"
+                label="Grayscale"
+                title="Grayscale (G)"
+                @click="toggleGrayscale"
+              />
+            </UIDropdownMenu>
+    </template>
     <template #right>
       <UIThemeChooser />
     </template>
