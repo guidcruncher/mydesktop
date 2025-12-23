4bf747f55e92e6258849f7f9e6d6e7d6fec71884
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-12-23 05:20:47 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "{" > /app/dist/public/config.json
 
-echo "  \"API_BASE_URL\": \"$API_BASE_URL\"," >> /app/dist/public/config.json
+echo "  \"API_BASE_URL\": \"$API_BASE_URL\"" >> /app/dist/public/config.json
 
 echo "}" >> /app/dist/public/config.json
 
