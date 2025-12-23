d940bb9f7a96257d71e88e9f67734d5bf7624d52
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-12-23 17:40:45 +0000

    fixs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "{" > /app/dist/public/config.json
 
-if [ "$NODE_ENV" == "production" ]: then
+if [ "$NODE_ENV" == "production" ]; then
 echo "  \"API_BASE_URL\": \"/\"" >>  /app/dist/public/config.json
 else
 echo "  \"API_BASE_URL\": \"$API_BASE_URL\"" >> /app/dist/public/config.json
